aac5e9c5a06bc112822c7a9c7a7ed6144eb917cc
SamarthGupta23 <125825832+SamarthGupta23@users.noreply.github.com>
2025-08-26 04:33:01 +0530

    Update grid.hpp

--- grid.hpp
+++ grid.hpp
@@ -1,22 +1,25 @@
 #include "coords.hpp"
 #include <bits/stdc++.h>
 #include <vector>
-const int width = 502;
-const int height = 502;
-double kinematicViscosity = 100;
-const int diffusionIterations = 40;
+extern const int width;
+extern const int height;
+extern const double kinematicViscosity;
+extern const int diffusionIterations;
+extern const int projectionIterations;
+extern const int dx;
 using namespace std;
 
 class grid {
-    public:
+public:
     vector <vector <Vec>> currentVelocities;
     vector <vector <Vec>> nextVelocities;
+    vector <vector <double>> pressureForces;
     double timeStep;
     double alpha;
 
 
     //core logic
-    void forces(Vec force);
+    void forces();
     void diffusion();
     void projection();
     void advection();
@@ -25,4 +28,6 @@ class grid {
     //helper functions
     void renderNext();
     void init();
+    Vec getBoundaryVelocity(int i, int j, const vector<vector<Vec>>& velocities);  //Neumann Zero Derivative assumption
+    double getBoundaryPressure(int i, int j, const vector<vector<double>> pressureForces);
 };
